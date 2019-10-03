c23d7e828c8cbd24c49c8b3a95369e49ad24faf3
Nguyen Tuan Anh <anhnt2@domain.vnext.vn>
2019-10-03 13:37:49 +0700

    add some awesome func

--- helper.js
+++ helper.js
@@ -1,23 +1,23 @@
 function parseURL(url) {
-    var a =  document.createElement('a');
-    a.href = url;
-    return {
-        host: a.hostname,
-        port: a.port,
-        query: a.search,
-        params: (function(){
-            var ret = {},
-                seg = a.search.replace(/^\?/,'').split('&'),
-                len = seg.length, i = 0, s;
-            for (;i<len;i++) {
-                if (!seg[i]) { continue; }
-                s = seg[i].split('=');
-                ret[s[0]] = s[1];
-            }
-            return ret;
-        })(),
-        hash: a.hash.replace('#','')
-    };
+	var a = document.createElement('a');
+	a.href = url;
+	return {
+		host: a.hostname,
+		port: a.port,
+		query: a.search,
+		params: (function () {
+			var ret = {},
+				seg = a.search.replace(/^\?/, '').split('&'),
+				len = seg.length, i = 0, s;
+			for (; i < len; i++) {
+				if (!seg[i]) { continue; }
+				s = seg[i].split('=');
+				ret[s[0]] = s[1];
+			}
+			return ret;
+		})(),
+		hash: a.hash.replace('#', '')
+	};
 }
 
 
@@ -34,3 +34,48 @@ function parseURL(url) {
 //   },
 //   "hash":"hashtag1"
 // }
+
+const isEmpty = prop =>
+	prop === null ||
+	prop === undefined ||
+	(prop.hasOwnProperty("length") && prop.length === 0) ||
+	(prop.constructor === Object && Object.keys(prop).length === 0);
+
+	
+const storage = {
+  get: (key) => {
+    const isClient = typeof window === 'object';
+    if (!isClient) {
+      return null
+    }
+    try {
+      return JSON.parse(localStorage.getItem(key))
+    } catch (e) {
+      return localStorage.getItem(key)
+    }
+  },
+  set: (key, value) => {
+    const finalValue = typeof value === 'object' ? JSON.stringify(value) : value.toString()
+    localStorage.setItem(key, finalValue)
+  },
+  remove: (key) => {
+    localStorage.removeItem(key)
+  }
+}
+
+const formatTextFloat = (
+  text,
+  count = 0,
+  displayNullWhenValueEqualZero = false,
+) => {
+  if (text) {
+    return text.toLocaleString('en', {
+      minimumFractionDigits: count,
+      maximumFractionDigits: count,
+    });
+  }
+  if (text === 0 && !displayNullWhenValueEqualZero) {
+    return parseFloat(text).toFixed(count);
+  }
+  return '--';
+}
